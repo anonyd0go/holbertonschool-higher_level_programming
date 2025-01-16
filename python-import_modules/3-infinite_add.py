d298bc134600ae20a426ef94b36d5041dc9a354b
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-16 15:36:39 -0400

    Add 3-infinite_add exercise with added try blocks

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        try:
+            sum += int(sys.argv[i])
+        except ValueError:
+            continue
+    print(sum)
