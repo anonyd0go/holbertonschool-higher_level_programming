bd1aa843977b2b4e90f31bb36bb70081150a5ea0
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-20 13:09:14 -0400

    Correct 5-no_c exercise

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
     list_str = list(my_string)
-    if list_str:
-        new_str = ""
-        i = 0
-        while i < len(list_str):
-            if list_str[i] == 'C' or list_str[i] == 'c':
-                del list_str[i]
-            i += 1
-        new_str = ''.join(list_str)
-        return new_str
-    return None
+    new_str = ""
+    i = 0
+    while i < len(list_str):
+        if list_str[i] == 'C' or list_str[i] == 'c':
+            del list_str[i]
+        i += 1
+    new_str = ''.join(list_str)
+    return new_str
