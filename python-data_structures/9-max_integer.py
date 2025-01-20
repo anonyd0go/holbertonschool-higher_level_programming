108de06d01b086aa5a125aa87883d6f60f280cf5
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-20 13:21:22 -0400

    Correct 9-max_int exercise

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max_num = 0
+        max_num = my_list[0]
         for num in my_list:
             if num > max_num:
                 max_num = num
