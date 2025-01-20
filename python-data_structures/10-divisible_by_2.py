b7cae405a862b56f0f9b248994f240fd0cd9b58a
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-20 13:36:47 -0400

    Chage 10-divisible_by_2 to list comprehension

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    is_div_list = []
-    for num in my_list:
-        is_div_list.append(num % 2 == 0)
+    is_div_list = [(num % 2 == 0) for num in my_list]
     return is_div_list
