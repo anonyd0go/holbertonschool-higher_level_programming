38e5a45cc4bfd71c2269215e94522bb5a3b826db
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-21 09:38:26 -0400

    Simplify 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_list = list(set(my_list))
+    unique = set(my_list)
     sum = 0
-    for num in unique_list:
+    for num in unique:
         sum += num
     return sum
