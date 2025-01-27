211d0609fbf521d44b70002a697da25d6d59a29a
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-27 14:37:13 -0400

    Add 2-square.py exercise

---
+++ python-classes/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square.
+"""
+
+
+class Square:
+    """
+    A class used to represent a Square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square with a given size.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
