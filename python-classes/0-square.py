fd2d241e5359579d82405434238ef0cf77741820
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-27 13:34:42 -0400

    Add 0-square.py exercise

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module serves as an introduction into OOP.
+
+It contains a simple class named Square.
+
+It has no attributes
+"""
+
+
+class Square:
+    """This is the simplest class you can make"""
+    pass
