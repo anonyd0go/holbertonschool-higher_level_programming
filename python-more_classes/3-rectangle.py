2d7f34a983722f99742fb2e11925dd79f78fea7e
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-01-29 08:55:17 -0400

    Correct pycodestyle for 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
 
     def __str__(self):
         """
-        Returns a string representation of the rectangle using the '#' character.
+        Returns a string representation of the rectangle using '#' char.
 
         Returns:
             str: The string representation of the rectangle.
