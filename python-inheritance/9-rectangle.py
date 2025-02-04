fee21fa550b1d171d9f681485dd2b101ee43ba2d
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-04 17:34:17 -0400

    Add 9-rectangle.py exercise

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+This module defines a class Rectangle that inherits from BaseGeometry.
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class used to represent a Rectangle, inheriting from BaseGeometry.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes the rectangle with a given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is not greater than 0.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            str: The string representation of the rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
