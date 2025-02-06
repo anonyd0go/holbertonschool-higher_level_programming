c22d41a566afac1cafc84286db9c43482d1f51d3
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-06 15:49:40 -0400

    Add task_05_dragon.py mixins exercise

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,19 +1,50 @@
 #!/usr/bin/python3
+"""
+This module defines mixin classes for swimming and flying abilities, and a Dragon class that uses these mixins.
+"""
 
 
 class SwimMixin:
+    """
+    A mixin class that provides swimming ability.
+    """
+
     def swim(self):
+        """
+        Prints that the creature swims.
+        """
         print("The crature swims!")
 
 
 class FlyMixin:
+    """
+    A mixin class that provides flying ability.
+    """
+
     def fly(self):
+        """
+        Prints that the creature flies.
+        """
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """
+    A class used to represent a Dragon, inheriting from SwimMixin and FlyMixin.
+
+    Methods:
+        roar: Prints that the dragon roars.
+        fire: Prints that the dragon breathes fire.
+    """
+
     def roar(self):
+        """
+        Prints that the dragon roars.
+        """
         print("The dragon roars!")
 
     def fire(self):
+        """
+        Prints that the dragon breathes fire.
+        """
         print("The dragon breathes fire!")
