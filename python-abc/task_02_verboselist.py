781834fe8d1a9ec87badbeea21707c3a0f044a11
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-06 11:49:23 -0400

    Add task_02_verboselist.py

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class VerboseList(list):
+    def append(self, data):
+        print("Added [{}] to the list.".format(data))
+        super().append(data)
+
+    def extend(self, data):
+        print("Extended the list with [{}] items.".format(len(data)))
+        super().extend(data)
+
+    def remove(self, data):
+        print("Removed [{}] from the list.".format(data))
+        super().remove(data)
+
+    def pop(self, index=-1):
+        popped = super().pop(index)
+        print("Popped [{}] from the list.".format(popped))
