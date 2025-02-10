ddde88752fd81f1da28887ad71ef1b097b29a3b3
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-10 16:46:08 -0400

    Add 4-from_json_string.py exercise

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns
+an object (Python data structure) represented by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to convert to an object.
+
+    Returns:
+        The object represented by the JSON string.
+    """
+    return json.loads(my_str)
