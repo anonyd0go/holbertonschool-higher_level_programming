0b43e050213a515dc0f262ead50b396f75cd0831
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-11 11:33:31 -0400

    Add 11-students.py exercise

---
+++ python-input_output/11-student.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+This module defines a class Student.
+"""
+
+
+class Student:
+    """
+    A class used to represent a Student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student with a first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+
+        Args:
+            attrs (list, optional): A list of attribute names
+            to include in the dictionary. Defaults to None.
+
+        Returns:
+            dict: The dictionary representation of the Student instance.
+        """
+        json_dict = {}
+        if attrs and isinstance(attrs, list):
+            for ele in attrs:
+                if ele in self.__dict__.keys():
+                    json_dict[ele] = self.__dict__[ele]
+            return json_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        with values from a JSON dictionary.
+
+        Args:
+            json (dict): The JSON dictionary to load data from.
+        """
+        for key, val in json.items():
+            self.__dict__[key] = val
