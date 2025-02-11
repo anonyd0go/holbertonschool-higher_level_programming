32107094e994b8abe1033ecb6eda0aefbe178013
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-11 10:53:41 -0400

    Add 9-student.py exercise

---
+++ python-input_output/9-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module defines a class Student.
+"""
+
+
+class Student:
+    """
+    A class used to represent a Student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student with a first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns the dictionary representation of the Student instance.
+
+        Returns:
+            dict: The dictionary representation of the Student instance.
+        """
+        return self.__dict__
