3c8969fb5229057efe9405d934f9c52e3c7fcaaf
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-14 11:18:01 -0400

    Correct return value of task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -14,15 +14,22 @@ def convert_csv_to_json(csv_file):
         csv_file (str): The name of the CSV file to convert.
 
     Returns:
-        None
+        bool: True if the conversion was successful, False otherwise.
     """
     csv_dat = []
     json_file = "data.json"
-    with open(csv_file, 'r', encoding="utf-8") as csv_handle:
-        csv_reader = csv.DictReader(csv_handle)
+    try:
+        with open(csv_file, 'r', encoding="utf-8") as csv_handle:
+            csv_reader = csv.DictReader(csv_handle)
 
-        for row in csv_reader:
-            csv_dat.append(row)
+            for row in csv_reader:
+                csv_dat.append(row)
+    except Exception as e:
+        return False
 
-    with open(json_file, 'w', encoding="utf-8") as json_handle:
-        json.dump(csv_dat, json_handle, indent=4)
+    try:
+        with open(json_file, 'w', encoding="utf-8") as json_handle:
+            json.dump(csv_dat, json_handle, indent=4)
+    except Exception as e:
+        return False
+    return True
