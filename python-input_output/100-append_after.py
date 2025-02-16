2da1208f7e2dfba58a75e37dad0faa971312bc6e
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-16 10:21:40 -0400

    Correct 100-append_after.py exercise

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,21 +7,22 @@ after each line containing a specific string.
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text to a file,
-    after each line containing a specific string.
+    Inserts a line of text to a file, after each line containing a specific
+    string.
 
     Args:
         filename (str): The name of the file.
         search_string (str): The string to search for within the file.
-        new_string (str): The string to insert after each line
-            containing the search string.
+        new_string (str): The string to insert after each line containing
+            the search string.
     """
-    file_lines = None
-    with open(filename, 'r', encoding='utf-8') as handler:
+    new_lines = []
+    with open(filename, 'r+', encoding='utf-8') as handler:
         file_lines = handler.readlines()
-    for i in range(len(file_lines)):
-        if search_string in file_lines[i]:
-            file_lines.insert(i + 1, new_string)
-    with open(filename, 'w', encoding='utf-8') as handler:
         for line in file_lines:
+            new_lines.append(line)
+            if search_string in line:
+                new_lines.append(new_string)
+        handler.seek(0)
+        for line in new_lines:
             handler.write(line)
