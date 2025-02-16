88b5fe5334a44a46a68a3fc25ec40f2eda0440a0
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-16 09:49:09 -0400

    Add 100-append_after.py exercise

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines a function that inserts a line of text to a file,
+after each line containing a specific string.
+"""
+from re import search
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file,
+    after each line containing a specific string.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert after each line
+            containing the search string.
+    """
+    file_lines = None
+    with open(filename, 'r', encoding='utf-8') as handler:
+        file_lines = handler.readlines()
+    for i in range(len(file_lines)):
+        if search(search_string, file_lines[i]):
+            file_lines.insert(i + 1, new_string)
+    with open(filename, 'w', encoding='utf-8') as handler:
+        for line in file_lines:
+            handler.write(line)
