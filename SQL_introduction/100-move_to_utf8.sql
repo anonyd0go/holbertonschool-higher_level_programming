836cb0610b7df94573c4a68e67c3c0a88831adc9
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-04 16:20:46 -0400

    Add comment to 100-move_to_utf8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
+-- Alter the database to utf8 format
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
