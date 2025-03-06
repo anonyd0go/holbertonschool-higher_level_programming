e1bd6988ca70d89407aa5fc323f75e33896be100
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-06 11:48:03 -0400

    Correct 16-shows_by_genre.sql query

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List all shows and their genre names
-SELECT title, tv_genres.name AS gen_name FROM tv_shows
+SELECT title, tv_genres.name AS name FROM tv_shows
 LEFT JOIN (
     tv_show_genres JOIN tv_genres
     ON tv_show_genres.genre_id = tv_genres.id
 ) ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title, gen_name;
+ORDER BY title, name;
