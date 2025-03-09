b33b6f01b3b5b14ab70cabbedae1d65be5350a25
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-09 15:14:13 -0400

    Add model_city.py exercise

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines a SQLAlchemy ORM model for the 'cities' table.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    A class used to represent a City.
+
+    Attributes:
+        __tablename__ (str): The name of the table in the database.
+        id (int): The primary key of the city.
+        name (str): The name of the city.
+        state_id (int): The foreign key referencing the state.
+        state (relationship): The relationship to the State model.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column("name", String(128))
+    state_id = Column(Integer, ForeignKey("states.id"))
+    state = relationship("State")
