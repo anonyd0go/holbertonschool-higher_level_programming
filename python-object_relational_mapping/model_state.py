67db5162ae87e0e64063025785c51ae76192c1d2
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-09 12:40:50 -0400

    Add model_state.py exercise

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a SQLAlchemy ORM model for the 'states' table.
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
+
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    A class used to represent a State.
+
+    Attributes:
+        __tablename__ (str): The name of the table in the database.
+        id (int): The primary key of the state.
+        name (str): The name of the state.
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column("name", String(128))
