ed06df25fc0ee0c28d99541ed3c005bb18722c4e
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-23 12:45:17 -0400

    Add task_02_logic.py

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    json_dict = None
+    with open("items.json", "r", encoding="utf-8") as jf:
+        json_dict = json.load(jf)
+    return render_template('items.html', items=json_dict["items"])
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
