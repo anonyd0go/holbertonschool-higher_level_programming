90886696d36a61bfe4016c64e68e8cfa2aea48ff
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-03 16:39:56 -0400

    Add 8-script.js

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+  .then((response) => response.json())
+  .then((data) => {
+    const helloDiv = document.querySelector('#hello');
+    helloDiv.textContent = data.hello;
+  })
+  .catch((error) => {
+    console.error('Error fetching translation:', error);
+  });
